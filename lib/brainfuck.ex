bcd92b32d56109f6188b1ca77a35f773fa210c18
dixx <jenzdixx@googlemail.com>
2018-02-16 21:58:18 +0100

    Add Brainfuck.run/1

--- lib/brainfuck.ex
+++ lib/brainfuck.ex
@@ -1,2 +1,16 @@
 defmodule Brainfuck do
+  @moduledoc """
+  Brainfuck interpreter written in Elixir.
+  """
+
+  @doc """
+  the main routine to run a Brainfuck program.
+  ## Examples
+    iex> Brainfuck.run("++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++.")
+    "Hello World!"
+  """
+  def run(program), do: run(program, 0, [0], "")
+
+  # no more Brainfuck found, output result and exit
+  defp run("", address, mem, output), do: {address, mem, output}
 end
--- test/brainfuck_test.exs
+++ test/brainfuck_test.exs
@@ -1,5 +1,8 @@
 defmodule BrainfuckTest do
   use ExUnit.Case
-  # doctest Brainfuck
+  # doctest Brainfuck # <-- skipped for now
 
+  test "returns nothing for an empty program" do
+    assert Brainfuck.run("") == {0, [0], ""}
+  end
 end
